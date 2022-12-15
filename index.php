a0fe36c9f5efe3deaeffc8e53cf3d222565791fb
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-12-15 16:17:47 +0100

    Crea functions.php e lo include nell' index.php

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php 
-    function RandomString($length=10)
-    {
-        $alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','L','M','N','O','P','Q','R','S','T','U','V','Z'];
-        $numbers=['0','1','2','3','4','5','6','7','8','9'];
-        $symbols=['@','#','§','*','/','&','%',"^",'(',')'];
-        $randstring = '';
-        while(strlen($randstring)<$length) {
-            $randstring .= $alphabet[rand(0, count($alphabet)-1)];
-            if(strlen($randstring)<$length){
-                $randstring .= $numbers[rand(0, count($numbers)-1)];
-                if(strlen($randstring)<$length){
-                    $randstring .= $symbols[rand(0, count($symbols)-1)];  
-                }   
-            }   
-        }
-        return $randstring;
-    }
+include __DIR__ ."/functions.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
